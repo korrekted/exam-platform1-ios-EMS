117b13a99b2d146f6348dce9f97be6e6a20fd268
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-10-04 17:35:13 +0300

    build 1

---
+++ EMS/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 247, green: 249, blue: 250)
+    
+    static let mainColor = UIColor(integralRed: 36, green: 106, blue: 252)
+    static let mainColorAlpha = UIColor(integralRed: 36, green: 106, blue: 252, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 5, green: 197, blue: 219)
+    
+    static let progress1Color = UIColor(integralRed: 36, green: 106, blue: 252)
+    static let progress2Color = UIColor(integralRed: 5, green: 197, blue: 219)
+    static let progress3Color = UIColor(integralRed: 229, green: 50, blue: 142)
+}
