c8e91bd50e0fecb7499fe3fb3e4728bdcaa6f0f1
akonst17 <akonst17@gmail.com>
2022-04-15 12:59:57 +0300

    upgrade; build 5

--- EMS/Classes/Misc/Appearance.swift
+++ EMS/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 36, green: 106, blue: 252)
     static let progress2Color = UIColor(integralRed: 5, green: 197, blue: 219)
     static let progress3Color = UIColor(integralRed: 229, green: 50, blue: 142)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- EMS/Classes/Service/Stats/Share/StatsShareManager.swift
+++ EMS/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ems.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ems.widget")
     }
     
     @discardableResult
