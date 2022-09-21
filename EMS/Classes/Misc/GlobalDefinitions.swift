b0bcf40b6407797b376669ac1985663fb78be3d9
akonst17 <akonst17@gmail.com>
2022-09-21 13:17:28 +0300

    интеграция sdk 2.0; новые урлы; build 8

--- EMS/Classes/Misc/GlobalDefinitions.swift
+++ EMS/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "2hdYj1t7BMz24M6f"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "8QnwIUOKx8sT6z2T"
 
     static let applicationTag = "EXAM_EMS"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "8QnwIUOKx8sT6z2T"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "4Rq07k7MGKezVhok"
 
     static let appStoreUrl = "https://apps.apple.com/app/ems-mobile-prep-app/id1586584604"
     static let appleAppID = "1586584604"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
